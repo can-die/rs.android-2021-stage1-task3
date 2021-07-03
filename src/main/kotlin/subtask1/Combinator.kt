ccde258d2a84689cbc49a80a289d5bc0109b293f
can-die <evgene@gmail.com>
2021-07-03 09:55:13 +0300

    https://github.com/can-die/rs.android-2021-stage1-task3

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,23 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
+    fun Int.factorial(): Long {
+        var res = 1L
+        for (i in 2..this) {
+            res = res * i
+        }
+        return res
+    }
+
+    fun getCombinations(from: Int, by: Int): Long {
+        return from.factorial() / ((from - by).factorial() * by.factorial())
+    }
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        for (i in 1..array[1]) {
+            if (getCombinations(array[1], i) >= array[0])
+                return i
+        }
+        return null
     }
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -2,8 +2,23 @@ package subtask2
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return getSquares(number, number.square())
+    }
+
+    private fun Int.square(): Long = (this.toLong() * this.toLong())
+
+    private fun getSquares(number: Int, total: Long): Array<Int>? {
+        for (i in number - 1 downTo 1) {
+            if (i.square() == total)
+                return Array(1) { i }
+
+            val diff = total - i.square()
+            if (diff > 0) {
+                val arr = getSquares(i, diff)
+                if (arr != null) return arr.plus(i)
+            }
+        }
+        return null
     }
 }
